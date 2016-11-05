7ed81e97fc3d11ec67d10dcbf64f1bec9e23ee7a
Gordon McNutt <gmcnutt@cableone.net>
2016-11-05 15:27:55 -0600

    Added sdl2-create/destroy-window

--- sdl2.c
+++ sdl2.c
@@ -3,6 +3,48 @@
 #include "scheme-private.h"
 
 
+#define scm_mk_ptr(sc, val) \
+	(sc)->vptr->mk_foreign_func((sc), (foreign_func)(val))
+#define scm_mk_integer(sc, val) (sc)->vptr->mk_integer((sc), (val))
+#define scm_mk_real(sc, val) (sc)->vptr->mk_real((sc), (val))
+#define scm_mk_symbol(sc, val) (sc)->vptr->mk_symbol((sc), (val))
+#define scm_mk_string(sc, val) (sc)->vptr->mk_string((sc), (val))
+
+#define scm_define(sc, sym, val)	 \
+        (sc)->vptr->scheme_define((sc),  \
+                                  (sc)->global_env,  \
+                                  (sc)->vptr->mk_symbol(sc, (sym)), \
+                                  (val))
+#define scm_define_int(sc, sym, val) \
+        scm_define(sc, sym, (sc)->vptr->mk_integer((sc), (val)))
+#define scm_define_bool(sc, sym, val) \
+        scm_define(sc, sym, (val) ? (sc)->T : (sc)->F)
+#define scm_define_ptr(sc, sym, val) \
+        scm_define(sc, sym, scm_mk_ptr(sc, val))
+#define scm_define_api_call(sc, sym, val) \
+        scm_define(sc, sym, scm_mk_ptr(sc, val))
+
+
+#define scm_is_pair(sc,arg) ((sc)->vptr->is_pair(arg))
+#define scm_is_num(sc, arg) ((sc)->vptr->is_number(arg))
+#define scm_is_int(sc, arg) ((sc)->vptr->is_integer(arg))
+#define scm_is_real(sc, arg) ((sc)->vptr->is_real(arg))
+#define scm_is_str(sc, arg) ((sc)->vptr->is_string(arg))
+#define scm_is_sym(sc, arg) ((sc)->vptr->is_symbol(arg))
+#define scm_is_ptr(sc, arg) ((sc)->vptr->is_foreign(arg))
+#define scm_is_closure(sc, arg) ((sc)->vptr->is_closure(arg))
+
+#define scm_car(sc, arg) ((sc)->vptr->pair_car(arg))
+#define scm_cdr(sc, arg) ((sc)->vptr->pair_cdr(arg))
+
+#define scm_str_val(sc, arg) ((sc)->vptr->string_value(arg))
+#define scm_ptr_val(sc, arg) ((void*)(arg)->_object._ff)
+#define scm_int_val(sc, arg) ((sc)->vptr->ivalue(arg))
+#define scm_real_val(sc, arg) ((sc)->vptr->rvalue(arg))
+#define scm_sym_val(sc, arg) ((sc)->vptr->symname(arg))
+#define scm_closure_code(sc, arg) ((sc)->vptr->closure_code(arg))
+#define scm_closure_env(sc, arg) ((sc)->vptr->closure_env(arg))
+
 /**
  * Initialize the SDL2 subsystem.
  */
@@ -14,9 +56,54 @@ static pointer sdl2_init(scheme *sc, pointer args)
 				"SDL_Init: %s\n", SDL_GetError());
 		return sc->NIL;
 	}
+
+	/* Cleanup SDL on exit. */
+	atexit(SDL_Quit);
+
 	return sc->T;
 }
 
+static pointer sdl2_create_window(scheme *sc, pointer args)
+{
+	SDL_Window *window;
+
+	/* Create the main window */
+	if (! (window = SDL_CreateWindow(
+		       "Demo", SDL_WINDOWPOS_UNDEFINED,
+		       SDL_WINDOWPOS_UNDEFINED, 640, 480,
+		       SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN))) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+			     "SDL_CreateWindow: %s\n", SDL_GetError());
+		return sc->NIL;
+	}
+
+	/* Return the window as a foreign pointer. */
+	return scm_mk_ptr(sc, window);
+}
+
+static pointer sdl2_destroy_window(scheme *sc, pointer args)
+{
+	SDL_Window *window=NULL;
+	pointer car=NULL;
+
+	if (! scm_is_pair(sc, args)) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+			     "%s: expected list of args\n", __FUNCTION__);
+		return sc->NIL;
+	}
+
+	car = scm_car(sc, args);
+
+	if (! scm_is_ptr(sc, car)) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+			     "%s: not a pointer\n", __FUNCTION__);
+		return sc->NIL;
+	}
+
+	window = (SDL_Window*)scm_ptr_val(sc, car);
+	SDL_DestroyWindow(window);
+	return sc->T;
+}
 
 /**
  * Initialize this dynamic extension.
@@ -29,7 +116,7 @@ static pointer sdl2_init(scheme *sc, pointer args)
  */
 void init_sdl2(scheme *sc)
 {
-	sc->vptr->scheme_define(
-		sc, sc->global_env, sc->vptr->mk_symbol(sc, "sdl2-init"),
-		sc->vptr->mk_foreign_func(sc, sdl2_init));
+	scm_define_api_call(sc, "sdl2-init", sdl2_init);
+	scm_define_api_call(sc, "sdl2-create-window", sdl2_create_window);
+	scm_define_api_call(sc, "sdl2-destroy-window", sdl2_destroy_window);
 }
