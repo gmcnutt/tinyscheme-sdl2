248e32c7b815b4851cd075c4533e55adf3888147
Gordon McNutt <gmcnutt@cableone.net>
2016-11-05 16:52:52 -0600

    Added window/renderer ctor/dtor and refactored logging.

--- sdl2.c
+++ sdl2.c
@@ -1,6 +1,6 @@
+#include "scheme-private.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_log.h>
-#include "scheme-private.h"
 
 
 #define scm_mk_ptr(sc, val) \
@@ -45,6 +45,35 @@
 #define scm_closure_code(sc, arg) ((sc)->vptr->closure_code(arg))
 #define scm_closure_env(sc, arg) ((sc)->vptr->closure_env(arg))
 
+
+static void log_critical(const char *fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+	SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION,
+			SDL_LOG_PRIORITY_CRITICAL, fmt, args);
+	va_end(args);
+}
+
+static void log_error(const char *fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+	SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION,
+			SDL_LOG_PRIORITY_ERROR, fmt, args);
+	va_end(args);
+}
+
+static void log_debug(const char *fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+	SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION,
+			SDL_LOG_PRIORITY_DEBUG, fmt, args);
+	va_end(args);
+}
+
+
 /**
  * Initialize the SDL2 subsystem.
  */
@@ -52,28 +81,33 @@ static pointer sdl2_init(scheme *sc, pointer args)
 {
 	/* Initialize the video, event, threading and file i/o subsystems. */
 	if (SDL_Init(SDL_INIT_VIDEO)) {
-		SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION,
-				"SDL_Init: %s\n", SDL_GetError());
+		log_critical("SDL_Init: %s\n", SDL_GetError());
 		return sc->NIL;
 	}
 
 	/* Cleanup SDL on exit. */
 	atexit(SDL_Quit);
 
+	/* Set logging level. */
+	SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION,
+			   SDL_LOG_PRIORITY_DEBUG);
+
 	return sc->T;
 }
 
+/**
+ * Wrapper for SDL_CreateWindow.
+ */
 static pointer sdl2_create_window(scheme *sc, pointer args)
 {
 	SDL_Window *window;
-
+	log_debug("sdl2_create_window:enter\n");
 	/* Create the main window */
 	if (! (window = SDL_CreateWindow(
 		       "Demo", SDL_WINDOWPOS_UNDEFINED,
 		       SDL_WINDOWPOS_UNDEFINED, 640, 480,
 		       SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN))) {
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-			     "SDL_CreateWindow: %s\n", SDL_GetError());
+		log_error("SDL_CreateWindow: %s\n", SDL_GetError());
 		return sc->NIL;
 	}
 
@@ -81,28 +115,89 @@ static pointer sdl2_create_window(scheme *sc, pointer args)
 	return scm_mk_ptr(sc, window);
 }
 
+/**
+ * Wrapper for SDL_DestroyWindow.
+ */
 static pointer sdl2_destroy_window(scheme *sc, pointer args)
 {
 	SDL_Window *window=NULL;
 	pointer car=NULL;
 
+	log_debug("sdl2_destroy_window:enter\n");
 	if (! scm_is_pair(sc, args)) {
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-			     "%s: expected list of args\n", __FUNCTION__);
+		log_error("%s: expected list of args\n", __FUNCTION__);
 		return sc->NIL;
 	}
 
 	car = scm_car(sc, args);
 
 	if (! scm_is_ptr(sc, car)) {
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-			     "%s: not a pointer\n", __FUNCTION__);
+		log_error("%s: not a pointer\n", __FUNCTION__);
 		return sc->NIL;
 	}
 
 	window = (SDL_Window*)scm_ptr_val(sc, car);
 	SDL_DestroyWindow(window);
 	return sc->T;
+}
+
+/**
+ * Wrapper for SDL_CreateRenderer.
+ */
+static pointer sdl2_create_renderer(scheme *sc, pointer args)
+{
+	SDL_Window *window=NULL;
+	SDL_Renderer *renderer=NULL;
+	pointer car=NULL;
+
+	log_debug("sdl2_create_renderer:enter\n");
+	if (! scm_is_pair(sc, args)) {
+		log_error("%s: expected list of args\n", __FUNCTION__);
+		return sc->NIL;
+	}
+
+	car = scm_car(sc, args);
+
+	if (! scm_is_ptr(sc, car)) {
+		log_error("%s: not a pointer\n", __FUNCTION__);
+		return sc->NIL;
+	}
+
+	window = (SDL_Window*)scm_ptr_val(sc, car);
+
+	if (! (renderer = SDL_CreateRenderer(window, -1, 0))) {
+		log_error("SDL_CreateRenderer: %s\n", SDL_GetError());
+		return sc->NIL;
+	}
+
+	return scm_mk_ptr(sc, renderer);
+}
+
+/**
+ * Wrapper for SDL_DestroyRenderer.
+ */
+static pointer sdl2_destroy_renderer(scheme *sc, pointer args)
+{
+	SDL_Renderer *renderer=NULL;
+	pointer car=NULL;
+
+	log_debug("sdl2_destroy_renderer:enter\n");
+	if (! scm_is_pair(sc, args)) {
+		log_error("%s: expected list of args\n", __FUNCTION__);
+		return sc->NIL;
+	}
+
+	car = scm_car(sc, args);
+
+	if (! scm_is_ptr(sc, car)) {
+		log_error("%s: not a pointer\n", __FUNCTION__);
+		return sc->NIL;
+	}
+
+	renderer = (SDL_Renderer*)scm_ptr_val(sc, car);
+	SDL_DestroyRenderer(renderer);
+	return sc->T;
+
 }
 
 /**
@@ -119,4 +214,8 @@ void init_sdl2(scheme *sc)
 	scm_define_api_call(sc, "sdl2-init", sdl2_init);
 	scm_define_api_call(sc, "sdl2-create-window", sdl2_create_window);
 	scm_define_api_call(sc, "sdl2-destroy-window", sdl2_destroy_window);
+	scm_define_api_call(sc, "sdl2-create-renderer",
+			    sdl2_create_renderer);
+	scm_define_api_call(sc, "sdl2-destroy-renderer",
+			    sdl2_destroy_renderer);
 }
