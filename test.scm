da5966b11e101006b260f450044c4b70e3cc6298
Gordon McNutt <gmcnutt@cableone.net>
2016-11-05 16:52:28 -0600

    Added 'with' macro and 'println'. Using them to test window and renderer creation/destruction.

--- test.scm
+++ test.scm
@@ -1,3 +1,24 @@
+;; Macro to emulate python's context managers. Turns this:
+;;   (with x ctor dtor ...)
+;; Into this:
+;;   (let ((x (ctor))) ... (dtor x))
+(define-macro (with v ctor dtor . form)
+  `(let ((,v ,ctor)) ,@form (,dtor ,v)))
+
+(define (newline)
+  (display "\n"))
+
+(define (println . args)
+  (map display args)
+  (newline))
+
+
 (load-extension "./sdl2")
 (sdl2-init)
-(display "ok\n")
+
+(with window (sdl2-create-window) sdl2-destroy-window
+      (println "window:" window)
+      (with renderer (sdl2-create-renderer window) sdl2-destroy-renderer
+	    (println "renderer:" renderer)
+	    )
+      )
