84f65997c935d64b257ec494fb503695c9f4f150
gmcnutt <gmcnutt@cableone.net>
2016-11-08 08:04:20 -0700

    Modified event handler to expect the event to be a list with the first element as the event type code

--- event-handling.scm
+++ event-handling.scm
@@ -9,13 +9,13 @@
 
 (define (lookup-event-handler event handlers)
   (cond ((null? handlers) '())
-        ((eqv? (caar handlers) event) (cdar handlers))
+        ((eqv? (caar handlers) (car event)) (cdar handlers))
         (else (lookup-event-handler event (cdr handlers)))))
 
 (define (handle-event event)
-  (if (null? event)
-      #t
-      (let ((handler (lookup-event-handler event *event-handlers*)))
-        (if (null? handler)
-            #t
-            (handler event)))))
+  (cond ((null? event) #t)
+        (else
+         (let ((handler (lookup-event-handler event *event-handlers*)))
+           (cond ((null? handler) #t)
+                 (else
+                  (apply handler event)))))))
